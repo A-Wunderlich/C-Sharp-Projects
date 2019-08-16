27cea29284bf01b2f7f29e3a361a3607db867185
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-08-15 20:50:52 -0700

    add blackjack console game

---
+++ TwentyOne/Casino/IWalkAway.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace casino.Interfaces
+{
+    interface IWalkAway
+    {
+        void WalkAway(Player player);
+
+    }
+}
